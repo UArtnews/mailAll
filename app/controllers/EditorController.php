b4c527d59bdad028ad83dcc6d9a3b2eb48a7129d
rnewsome <rnewsome@akron.edu>
2019-01-17 10:01:52 -0500

    Updated Style for title

--- app/controllers/EditorController.php
+++ app/controllers/EditorController.php
@@ -53,7 +53,7 @@ class EditorController extends \BaseController
 		$parameters['data']['h1fontsize'] = $util->getTweakableByParam("publication-h1-font-size", $parameters['data']['tweakables']);	
 		$parameters['data']['h1fontweight'] = $util->getTweakableByParam("publication-h1-font-weight", $parameters['data']['tweakables']);
 		$parameters['data']['h1font'] = $util->getTweakableByParam("publication-h1-font", $parameters['data']['tweakables']);
-		$parameters['data']['h1style'] = ' Style="color: ' . $parameters['data']['h1color'] . '; font-size: ' . $parameters['data']['h1fontsize'] . ';  font-weight: bold; font-family:'. str_replace('"', '', $parameters['data']['h1font']) . '!important "';
+		$parameters['data']['h1style'] = ' Style="color: ' . $parameters['data']['h1color'] . '; font-size: ' . $parameters['data']['h1fontsize'] . ';  font-weight: '  . $parameters['data']['h1fontweight'] .  '; font-family:'. str_replace('"', '', $parameters['data']['h1font']) . '!important "';
 		//print( $parameters['data']['tweakables'][0]->id . "font-size");
 		
 		
